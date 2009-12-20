3c9c6a1ea5c6435ad37c1d71ca0e508733219e58
fabiopereira.me <fabiopereira.me@f374a044-e7cc-11de-83d8-6d5adf01d787>
2009-12-20 10:32:16 +0000

    Adding JDO features

---
+++ trunk/src/me/fabiopereira/eurotrialsmonitor/controller/MonitorController.java
@@ -0,0 +1,39 @@
+package me.fabiopereira.eurotrialsmonitor.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import me.fabiopereira.eurotrialsmonitor.model.Monitor;
+import me.fabiopereira.eurotrialsmonitor.repository.MonitorRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/monitor")
+public class MonitorController {
+
+	private final MonitorRepository monitorRepository;
+	
+	@Autowired
+	public MonitorController(MonitorRepository monitorRepository) {
+		this.monitorRepository = monitorRepository;
+	}
+	
+    @RequestMapping(method = RequestMethod.GET)
+	public void monitorLoginGet() {
+	}
+
+    @RequestMapping(method = RequestMethod.POST)
+	public String monitorLogin(@RequestParam("id") String id, HttpServletRequest request) {
+		Monitor monitor = monitorRepository.findById(id);
+		if (monitor != null) {
+			request.getSession().setAttribute("monitor", monitor);
+			return "redirect:questionnaire";
+		}
+		return null;
+	}
+	
+}
--- trunk/src/me/fabiopereira/eurotrialsmonitor/repository/MonitorRepository.java
+++ trunk/src/me/fabiopereira/eurotrialsmonitor/repository/MonitorRepository.java
@@ -1,8 +1,21 @@
 package me.fabiopereira.eurotrialsmonitor.repository;
 
+import java.util.Collection;
+
+import javax.jdo.JDOException;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import org.springframework.orm.jdo.JdoCallback;
+
 import me.fabiopereira.eurotrialsmonitor.model.Monitor;
+import me.fabiopereira.eurotrialsmonitor.model.Monitors;
 
 public interface MonitorRepository {
 
 	Monitor findById(String id);
+
+	Monitors findAll();
+	
+	void add(Monitor monitor);
 }
